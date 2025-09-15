6d242e3e6cbd8c9763c95d4959217153e886cd4e
nicolas correa pulido <nicolascorreapulido@gmail.com>
2025-09-15 16:49:24 -0500

    Creacion de la clase contacts y creacion del primer metodo de instancia

--- contactbook/model/contacts.py
+++ contactbook/model/contacts.py
@@ -1,1 +1,12 @@
-# TODO: Add code here
+class Contact:
+    def __init__(self, name: str, phone: str, email: str):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.tags = []
+
+
+    def add_tag(self, tag:str):
+        if tag in self.tags:
+            raise ValueError("Tag already exists")
+        self.tags.append(tag)
