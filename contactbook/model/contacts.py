9b09714ccc6c81691178e1595c89774a938c70f2
nicolas correa pulido <nicolascorreapulido@gmail.com>
2025-09-15 17:01:29 -0500

    Creacion de el metodo str, solucionar problema invocando date time

--- contactbook/model/contacts.py
+++ contactbook/model/contacts.py
@@ -1,12 +1,18 @@
+
+
 class Contact:
-    def __init__(self, name: str, phone: str, email: str):
+    def __init__(self, name: str, phone: str, email: str,):
         self.name = name
         self.phone = phone
         self.email = email
-        self.tags = []
+        self.tags: list[str] = []
+        self.date: datetime = datetime.now()
 
 
     def add_tag(self, tag:str):
         if tag in self.tags:
             raise ValueError("Tag already exists")
         self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\n,Phone: {self.phone}\n,Email: {self.email}\n,Tags: {self.tags}\n,Created on: {self.creation_date}"
